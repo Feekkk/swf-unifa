eb791772c336af80346e1b1b2d6213754d8e8e9b
Wan Afiq <zxduke350@gmail.com>
2025-11-03 10:30:12 +0800

    make admin table and seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'name' => 'Admin',
+            'email' => 'admin@unikl.com',
+            'password' => Hash::make('admin123'),
+        ],
+        [
+            'name' => 'Admin 2',
+            'email' => 'admin@unikl.com',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
